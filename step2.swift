b678dac0cf23c39e5f306605990ba64bf179419f
James Abela <abela.james@gmail.com>
2022-06-28 11:49:51 +0800

    Create step2.swift

---
+++ step2.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+func RandomPrompt() -> String{
+    let things = ["Three people you enjoyed talking to today", "Three things you are grateful for today", "Three moments that made you smile today", "Three best things that have happened today"]
+    return String(things.randomElement()!)
+}
+
+struct ContentView: View {
+    @State var GoodThing1: String = ""
+    @State var GoodThing2: String = ""
+    @State var GoodThing3: String = ""
+    @State var GoodThingPrompt: String = RandomPrompt()
+    
+    var body: some View {
+        VStack {
+            Text(GoodThingPrompt)
+            TextField("1st", text: $GoodThing1)
+            TextField("2nd", text: $GoodThing2)
+            TextField("3rd", text: $GoodThing3)
+        }
+    }
+}
